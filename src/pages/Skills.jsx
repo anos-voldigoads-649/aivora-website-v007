135e780466ec16c7923bb7e12b751fe7cd5c111f
Anos <anosvoldigoadw1@gamil.com>
2025-12-17 21:53:13 +0530

    Fix Skills.jsx and update all changes

--- src/pages/Skills.jsx
+++ src/pages/Skills.jsx
@@ -1,7 +1,7 @@
 import React, { useEffect, useState } from "react";
 import Navbar from "../components/Navbar";
 import { FiCheckCircle, FiAward } from "react-icons/fi";
-import "./skills.css";
+import "./Skills.css";
 
 export default function Skills() {
   const [field, setField] = useState("");
